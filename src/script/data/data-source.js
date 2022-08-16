168e6945b2e8f8c57f85c510c8c1116760707a23
adindaafebri <111365009+adindaafebri@users.noreply.github.com>
2022-08-16 21:26:51 +0700

    Add files via upload

---
+++ src/script/data/data-source.js
@@ -0,0 +1,19 @@
+import cats from "./cats.js";
+
+class DataSource{
+  static searchCat(keyword) {
+    return fetch(` https://api.thecatapi.com/v1/images/search?limit=10${keyword}`)
+    .then(response => {
+      return response.json();
+    })
+    .then(respoonseJson => {
+      if (respoonseJson.v1){
+        return Promise.resolve(respoonseJson.v1);
+      }else{
+        return Promise.reject('${keyword} is not found');
+      }
+    });
+  }
+}
+
+export default DataSource;
